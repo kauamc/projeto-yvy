0e362e44841d6d293e965156ac45e8392a91e88b
Kauã Cavalcante <cavalcante.kauamoraes@gmail.com>
2026-04-15 21:12:46 -0300

    btn funcionando com js

---
+++ modelo/script.js
@@ -0,0 +1,7 @@
+function mostrarInfoExtras() {
+    const infoExtras = document.querySelector('#info-extras')
+    const seta = document.querySelector('#seta-info-extra')
+
+    infoExtras.style.display = infoExtras.style.display === 'grid' ? 'none' : 'grid'
+    seta.style.transform = infoExtras.style.display === 'grid' ? 'rotate(180deg)' : 'rotate(0deg)'
+}
