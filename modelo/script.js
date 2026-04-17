ea02477824d5914c7d881cb7c2d2f068d0600a05
Kauã Cavalcante <cavalcante.kauamoraes@gmail.com>
2026-04-17 08:52:18 -0300

    svg posicao arrumada

--- modelo/script.js
+++ modelo/script.js
@@ -6,7 +6,7 @@ function mostrarInfoExtras() {
     seta.style.transform = infoExtras.style.display === 'grid' ? 'rotate(180deg)' : 'rotate(0deg)'
 }
 
-fetch('territorio1.svg')
+fetch('territorio3.svg')
     .then(resposta => resposta.text())
     .then(svg => {
         document.querySelector('#territorio').innerHTML = svg
