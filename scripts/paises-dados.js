f9e83555970c80f36498125f8c1b60803a771cd2
Kauã Cavalcante <cavalcante.kauamoraes@gmail.com>
2026-04-21 14:54:12 -0300

    paises organizados nas pastas corretas e turquia criada

--- scripts/paises-dados.js
+++ scripts/paises-dados.js
@@ -1,49 +1,8 @@
-function mostrarInfoExtras() {
-    const infoExtras = document.querySelector('#info-extras')
-    const seta = document.querySelector('#seta-info-extra')
-
-    infoExtras.style.display = infoExtras.style.display === 'grid' ? 'none' : 'grid'
-    seta.style.transform = infoExtras.style.display === 'grid' ? 'rotate(180deg)' : 'rotate(0deg)'
-}
-
-fetch('../componentes/corpo-pais.html')
-    .then(res => res.text())
-    .then(corpo => {
-        document.getElementById('corpo').innerHTML = corpo
-
-        fetch("territorio3.svg")
-            .then(resposta => resposta.text())
-            .then(svg => {
-                document.querySelector('#territorio').innerHTML = svg
-
-                var mapa = document.getElementById("mapa")
-                var info = document.getElementById("info")
-
-                mapa.addEventListener('click', function(evento) {
-                    var outrosEstados = mapa.querySelectorAll('path')
-                    outrosEstados.forEach(function(estado){
-                        estado.style.fill = "lightgrey"
-                    })
-
-                    var pathClicado = evento.target
-                    var estado = pathClicado.closest('path')
-
-                    if(!estado){
-                        return
-                    }
-
-                    var nome = estado.getAttribute('data-name')
-                    info.innerHTML= `<p>${nome}</p>`
-                    estado.style.fill = "grey"
-                })
-            })
-    })
-
 // Troca de informações
-    const ID = "br"
+    const ID = document.getElementById('p-id').textContent
 // Troca de informações
 
-fetch('dados.json')
+fetch('../dados.json')
     .then(res => res.json())
     .then(dados => {
         const pais = dados.find(p => p.id === ID)
---
+++ scripts/paises-geral.js
@@ -0,0 +1,41 @@
+function mostrarInfoExtras() {
+    const infoExtras = document.querySelector('#info-extras')
+    const seta = document.querySelector('#seta-info-extra')
+
+    infoExtras.style.display = infoExtras.style.display === 'grid' ? 'none' : 'grid'
+    seta.style.transform = infoExtras.style.display === 'grid' ? 'rotate(180deg)' : 'rotate(0deg)'
+}
+
+fetch('../../componentes/corpo-pais.html')
+    .then(res => res.text())
+    .then(corpo => {
+        document.getElementById('corpo').innerHTML = corpo
+
+        fetch("territorio.svg")
+            .then(resposta => resposta.text())
+            .then(svg => {
+                document.querySelector('#territorio').innerHTML = svg
+
+                var mapa = document.getElementById("mapa")
+                var info = document.getElementById("info")
+
+                mapa.addEventListener('click', function(evento) {
+                    var outrosEstados = mapa.querySelectorAll('path')
+                    outrosEstados.forEach(function(estado){
+                        estado.style.fill = "lightgrey"
+                    })
+
+                    var pathClicado = evento.target
+                    var estado = pathClicado.closest('path')
+
+                    if(!estado){
+                        return
+                    }
+
+                    var nome = estado.getAttribute('data-name')
+                    info.innerHTML= `<p>${nome}</p>`
+                    estado.style.fill = "grey"
+                })
+            })
+    })
+
